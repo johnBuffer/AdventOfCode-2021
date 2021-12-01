fc170557639781b7977a7ff70d977caed12523c8
Jean Tampon <jean.tampon@gmail.com>
2021-12-01 14:07:38 +0100

    Add day 01

--- 01/main.py
+++ 01/main.py
@@ -7,7 +7,7 @@ def solve_1(data):
 
 
 def solve_2(data):
-    return sum([sum(data[i+1:i+4]) > sum(data[i:i+3]) for i in range(1, len(data) - 2)])
+    return sum([sum(data[i:i+3]) > sum(data[i-1:i+2]) for i in range(1, len(data) - 2)])
 
 
 print(solve_1(data))
