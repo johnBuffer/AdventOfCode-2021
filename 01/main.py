0a811f5d07dc6e8fab44cd2e7a8b3dd27841e28e
Jean Tampon <jean.tampon@gmail.com>
2021-12-01 10:12:16 +0100

    Add day 01

---
+++ 01/main.py
@@ -0,0 +1,14 @@
+file_data = open('input.txt').read().split('\n')
+data = [int(l) for l in file_data]
+
+
+def solve_1(data):
+    return sum([data[i + 1] > data[i] for i, _ in enumerate(data[1:])])
+
+
+def solve_2(data):
+    return sum([sum(data[i:i+3]) > sum(data[i-1:i+2]) for i, _ in enumerate(data[1:-2])])
+
+
+print(solve_1(data))
+print(solve_2(data))
