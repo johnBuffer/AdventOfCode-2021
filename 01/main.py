39ec073c4fe39335eb0c25ff364e4842a4941ffd
Jean Tampon <jean.tampon@gmail.com>
2021-12-01 12:41:08 +0100

    Add day 01

--- 01/main.py
+++ 01/main.py
@@ -3,11 +3,11 @@ data = [int(l) for l in file_data]
 
 
 def solve_1(data):
-    return sum([data[i + 1] > data[i] for i, _ in enumerate(data[1:])])
+    return sum([data[i] > data[i-1] for i in range(1, len(data))])
 
 
 def solve_2(data):
-    return sum([sum(data[i+1:i+4]) > sum(data[i:i+3]) for i, _ in enumerate(data[1:-2])])
+    return sum([sum(data[i+1:i+4]) > sum(data[i:i+3]) for i in range(1, len(data) - 2)])
 
 
 print(solve_1(data))
