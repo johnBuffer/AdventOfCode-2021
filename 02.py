44cf20010570aa573a624e1158addd9c6782e801
Jean Tampon <jean.tampon@gmail.com>
2021-12-02 09:22:05 +0100

    Add day 02

--- 02.py
+++ 02.py
@@ -15,9 +15,9 @@ def solve_1():
 def solve_2():
     x, y, a, c = 0, 0, 0, {'up': (0, 0, -1), 'down': (0, 0, 1), 'forward': (1, 1, 0)}
     for instr, value in [(i, int(v)) for i, v in [l.split(' ') for l in data]]:
-            x += value * c[instr][0]
-            y += value * c[instr][1] * a
-            a += value * c[instr][2]
+        x += value * c[instr][0]
+        y += value * c[instr][1] * a
+        a += value * c[instr][2]
     return x * y
 
 
