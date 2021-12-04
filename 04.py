01dcbad6702bb3ee1bd274f1975f63e807c21899
Jean Tampon <jean.tampon@gmail.com>
2021-12-04 09:10:26 +0100

    Add day 04

--- 04.py
+++ 04.py
@@ -1,12 +1,9 @@
 from common.input_loader import load_input
 
 data = load_input(4)
+win = lambda g, n: any(set(g[i:i+5]).issubset(n) for i in range(0, 25, 5)) or any(set(g[i::5]).issubset(n) for i in range(0, 5)) # LEL
 NS = [int(i) for i in data[0].split(',')]
 GS = [[int(c) for c in ' '.join(data[i:i+6][:5]).split(' ') if len(c)] for i in range(2, len(data), 6)]
-
-
-def win(g, n):
-    return any(set(g[i:i+5]).issubset(n) for i in range(0, 25, 5)) or any(set(g[i::5]).issubset(n) for i in range(0, 5))
 
 
 def solve(gs = GS, s = [], n = set(NS[:5]), i = 5):
