6302cbc35921dfa55c30e49c133d9cb1999e3198
Jean Tampon <jean.tampon@gmail.com>
2021-12-04 10:43:20 +0100

    Update 04.py

--- 04.py
+++ 04.py
@@ -6,7 +6,7 @@ NS = [int(i) for i in data[0].split(',')]
 GS = [[int(c) for c in ' '.join(data[i:i+6][:5]).split(' ') if len(c)] for i in range(2, len(data), 6)]
 
 
-def solve(gs = GS, s = [], n = set(NS[:5]), i = 5):
+def solve(gs = GS, s = [], n = set(), i = 0):
     return s if i == len(NS) else solve([g for g in gs if not win(g, n)], s + [NS[i-1] * sum(set(g) - n) for g in gs if win(g, n)], n|{NS[i]}, i+1)
 
 
