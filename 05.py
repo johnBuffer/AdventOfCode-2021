691bd8753a3994c3253a83d813aed502d225af6d
Jean Tampon <jean.tampon@gmail.com>
2021-12-05 10:56:54 +0100

    Update 05.py

--- 05.py
+++ 05.py
@@ -5,12 +5,12 @@ LS = [list(map(lambda x: int(x), l.replace(' -> ', ',').split(','))) for l in lo
 sign = lambda x1, x2: 1 if x1 < x2 else -1 if x1 > x2 else 0
 
 
-def draw(x1, y1, x2, y2):
-    return [(x1, y1)] + (draw(x1 + sign(x1, x2), y1 + sign(y1, y2), x2, y2) if x1!=x2 or y1!=y2 else [])
+def pts(x1, y1, x2, y2):
+    return [(x1, y1)] + (pts(x1 + sign(x1, x2), y1 + sign(y1, y2), x2, y2) if x1!=x2 or y1!=y2 else [])
 
 
 def solve(grid, filter):
-    for x, y in sum((draw(*l) for l in LS if filter(*l)), []): grid[(x, y)] += 1
+    for x, y in sum((pts(*l) for l in LS if filter(*l)), []): grid[(x, y)] += 1
     return sum(1 for v in grid.values() if v > 1)
 
 
