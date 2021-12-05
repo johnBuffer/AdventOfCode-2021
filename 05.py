13ef9fc7c899b77cdbd7e63e6fc9f1cb12ce00a1
Jean Tampon <jean.tampon@gmail.com>
2021-12-05 10:47:13 +0100

    Add day 05

---
+++ 05.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from common.input_loader import load_input
+
+LS = [list(map(lambda x: int(x), l.replace(' -> ', ',').split(','))) for l in load_input(5)]
+sign = lambda x1, x2: 1 if x1 < x2 else -1 if x1 > x2 else 0
+
+
+def draw(x1, y1, x2, y2):
+    return [(x1, y1)] + (draw(x1 + sign(x1, x2), y1 + sign(y1, y2), x2, y2) if x1!=x2 or y1!=y2 else [])
+
+
+def solve(grid, filter=None):
+    for x, y in sum((draw(*l) for l in LS if not filter or filter(*l)), []): grid[(x, y)] += 1
+    return sum(1 for v in grid.values() if v > 1)
+
+
+print(solve(defaultdict(int), lambda x1, y1, x2, y2: x1 == x2 or y1 == y2))
+print(solve(defaultdict(int)))
