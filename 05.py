11bc040c55a858e68940a1026d5813df8a05bcc4
Jean Tampon <jean.tampon@gmail.com>
2021-12-05 10:53:29 +0100

    Update 05.py

--- 05.py
+++ 05.py
@@ -9,10 +9,10 @@ def draw(x1, y1, x2, y2):
     return [(x1, y1)] + (draw(x1 + sign(x1, x2), y1 + sign(y1, y2), x2, y2) if x1!=x2 or y1!=y2 else [])
 
 
-def solve(grid, filter=None):
-    for x, y in sum((draw(*l) for l in LS if not filter or filter(*l)), []): grid[(x, y)] += 1
+def solve(grid, filter):
+    for x, y in sum((draw(*l) for l in LS if filter(*l)), []): grid[(x, y)] += 1
     return sum(1 for v in grid.values() if v > 1)
 
 
 print(solve(defaultdict(int), lambda x1, y1, x2, y2: x1 == x2 or y1 == y2))
-print(solve(defaultdict(int)))
+print(solve(defaultdict(int), lambda x1, y1, x2, y2: True))
