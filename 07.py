1b5ff007bed72f34c7a7dc6511abe9969aab4055
Jean Tampon <jean.tampon@gmail.com>
2021-12-07 07:38:18 +0100

    Add day 07

--- 07.py
+++ 07.py
@@ -1,7 +1,7 @@
 from common.input_loader import load_input
 
 numbers = [int(i) for i in load_input(7)[0].split(',')]
-def solve(n, f): return min(sum(f(abs(i - v)) for i in n) for v in range(len(n)))
+def solve(n, f): return min(sum(f(abs(i - v)) for i in n) for v in range(max(n)))
     
 print(solve(numbers, lambda x: x))
 print(solve(numbers, lambda x: int(x*(x+1)/2)))
