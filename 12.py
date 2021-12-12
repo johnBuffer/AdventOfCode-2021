0989e903dfab2c112128511ba60325e708c07e76
Jean Tampon <jean.tampon@gmail.com>
2021-12-12 12:10:05 +0100

    Update 12.py

--- 12.py
+++ 12.py
@@ -3,7 +3,7 @@ from common.input_loader import load_input
 
 data = [(a, b) for a, b in [l.split('-') for l in load_input(12)]]
 M = {a: set(e for s, e in data if s == a) | set(s for s, e in data if e == a) for a in set(v[0] for v in data)}
-get_nxt = lambda a, d, cs :{k: [c for c in v if c != a or a == d or not c.islower()] for k, v in cs.items()}
+get_nxt = lambda a, d, cs: {k: [c for c in v if c != a or a == d or not c.islower()] for k, v in cs.items()}
 
 
 def walk(cs, a='start', d='', p=''):
