6b0b4f5a686161542188ea1362540c6e3d692735
Jean Tampon <jean.tampon@gmail.com>
2021-12-14 09:29:17 +0100

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,9 +1,7 @@
 from common.input_loader import load_input
 from collections import defaultdict, Counter
 
-
 data = load_input(14)
-template = data[0]
 INS = {a: b for a, b in [l.split(' -> ') for l in data[2:]]}
 
 
@@ -17,8 +15,8 @@ def process(chain, count):
 
 
 def solve(iter):
-    d, count = defaultdict(int), defaultdict(int, Counter(template))
-    for i in range(len(template) - 1): d[template[i:i+2]] += 1
+    d, count = defaultdict(int), defaultdict(int, Counter(data[0]))
+    for i in range(len(data[0]) - 1): d[data[0][i:i+2]] += 1
     for _ in range(iter): d = process(d, count)
     return (max(count.values()) - min(count.values()))
 
