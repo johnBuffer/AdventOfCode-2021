600980850a774ed02204f0044f7faebfe12c3e53
Jean Tampon <jean.tampon@gmail.com>
2021-12-18 11:05:03 +0100

    Add day 18

---
+++ 18.py
@@ -0,0 +1,73 @@
+import math
+from common.input_loader import load_input
+
+
+def find_coma(x, d=0, r=1):
+    return r if (x[0] == ',' and d == 0) else find_coma(x[1:], d + (x[0] == '[') - (x[0] == ']'), r + 1)
+
+
+def parse_num(l, d=0, id=''):
+    if l[0] != '[': return [(int(l), id)]
+    coma = find_coma(l[1:-1])
+    return parse_num(l[1:coma], d+1, id+'l') + parse_num(l[coma+1:-1], d+1, id+'r')
+
+
+def explode(x):
+    for i, (n, id) in enumerate(x):
+        if len(id) > 4 and is_left(x, i):
+                n_right, id_right = x[i+1]
+                if i > 0:
+                    n2, id2 = x[i-1]
+                    x[i-1] = (n2+n, id2)
+                if i < len(x) - 2:
+                    n2, id2 = x[i+2]
+                    x[i+2] = (n_right+n2, id2)
+                x.remove((n_right, id_right))
+                x[i] = (0, id[:-1])
+                return True
+    return False
+
+
+def split(x):
+    for i, (n, id) in enumerate(x):
+        if n > 9:
+            x.remove((n, id))
+            x.insert(i, (n//2, id+'l'))
+            x.insert(i+1, (int(math.ceil(n/2)), id+'r'))
+            return True
+    return False
+
+
+def is_left(x, i):
+    if i < len(x)-1:
+        n_1, id_1 = x[i]
+        n_2, id_2 = x[i+1]
+        if len(id_1) == len(id_2):
+            return id_1[:-1] == id_2[:-1] and id_1[-1] == 'l' and id_2[-1] == 'r' and isinstance(n_1, int) and isinstance(n_2, int)
+    return False
+
+
+def calc_ampl(x):
+    while len(x) > 1:
+        for i, (n, id) in enumerate(x):
+            if is_left(x, i):
+                n_right, id_right = x[i+1]
+                x.remove((n, id))
+                x.remove((n_right, id_right))
+                x.insert(i, (3*n + 2*n_right, id[:-1]))
+    return x[0][0]
+
+
+def add(x, y):
+    o, stop = [(n, 'l' + id) for n, id in x] + [(n, 'r' + id) for n, id in y], False
+    while not stop: stop = (not explode(o) and not split(o))
+    return o
+
+
+def solve_1(d, res):
+    return calc_ampl(res) if len(d) == 0 else solve_1(d[1:], add(res, d[0]))
+
+
+data = [parse_num(l) for l in load_input(18)]
+print(solve_1(data[1:], data[0]))
+print(max(calc_ampl(add(d1, d2)) for i, d1 in enumerate(data) for j, d2 in enumerate(data) if j != i))
