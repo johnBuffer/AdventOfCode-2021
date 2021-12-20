7caaa62c09281e42c3e2d02e460ec0fa40ac29a5
Jean Tampon <jean.tampon@gmail.com>
2021-12-20 11:29:51 +0100

    Update 20.py

--- 20.py
+++ 20.py
@@ -2,8 +2,8 @@ from common.input_loader import load_input
 
 data = load_input(20)
 A, pict = [str(int(c == '#')) for c in data[0]], {(x, y): str(int(c == '#')) for y, l in enumerate(data[2:]) for x, c in enumerate(l)}
-def bnd(d, op, i=0): return op(k[i] for k in d) + (2 if op == max else -1)
-def apply(p, d): return {(x, y): A[int(''.join(p.get((ox, oy), d) for oy in range(y-1, y+2) for ox in range(x-1, x+2)), 2)] for y in range(bnd(p, min, 1), bnd(p, max, 1)) for x in range(bnd(p, min), bnd(p, max))}
+def bnd(d, i=0): return range(min(k[i] for k in d) - 1, max(k[i] for k in d) + 2)
+def apply(p, d): return {(x, y): A[int(''.join(p.get((ox, oy), d) for oy in range(y-1, y+2) for ox in range(x-1, x+2)), 2)] for y in bnd(p, 1) for x in bnd(p)}
 def enhance(p, count, i=0): return p if count == 0 else enhance(apply(p, str(i%2)), count-1, i+1)
 
 
